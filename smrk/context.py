e18d6478251494cd55a540fa594a23c88d2b6ff8
Kube Kubow <kubow@users.noreply.github.com>
2023-10-31 21:54:18 +0100

    context file update feat geopy

--- smrk/context.py
+++ smrk/context.py
@@ -1,20 +1,30 @@
 from datetime import datetime
 from dateutil.parser import parse
+from geopy.geocoders import Nominatim
+from geopy.location import Location
+
 
 class Actual:
-    def __init__(self, content: str="", t: str="time") -> None:
-        if t=="time":
-            # osetrit cas
+    # Universal holder for time & position
+    def __init__(self, content: str = "", t: str = "time") -> None:
+        if t == "time":
+            self.service = None
             if isinstance(content, str) and len(content) > 0:
                 self.value = parse(content)
             elif isinstance(content, datetime):
                 self.value = content
             else:
                 self.value = datetime.now()
-        elif t=="place":
-            # osetrit polohu
+        elif t == "place":
+            self.service = Nominatim(user_agent="astro")
             if isinstance(content, str):
-                self.value = content
+                self.move_around_globe(content)
+
+    def __str__(self):
+        if isinstance(self.value, Location):
+            return self.value.address
+        else:
+            return str(self.value)
 
     def add_some_time(self, of):
         if isinstance(of, str):
@@ -22,15 +32,15 @@ class Actual:
         elif isinstance(of, datetime):
             self.value = datetime.combine(self.value, of)
 
-    def move_around_globe(self, city: str="Prague"):
+    def move_around_globe(self, city: str = "Prague"):
         if isinstance(city, str):
-            self.value = city
+            self.value = self.service.geocode(city)
 
 
 if __name__ == "__main__":
     # simple test
-    a = Actual("15.5.2020")
-    
-    a = Actual("11/9/1982")
+    print(Actual("15.5.2020"))
 
-    a = Actual("Prague", t="place")
+    print(Actual("11/9/1982"))
+
+    print(Actual("Prague", t="place"))
