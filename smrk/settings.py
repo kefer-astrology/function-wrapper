45d4ff86a629513c2d566c22570b7b50d38293f5
Kube Kubow <kubow@users.noreply.github.com>
2023-11-12 15:22:26 +0100

    translation and settings outsourced (sqlite)

---
+++ smrk/settings.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sqlite3
+from pathlib import Path
+
+
+def settings_file() -> Path:
+    temp = Path(__file__).parent  # db file stored in the same folder
+    print(f"Returned value: {temp / 'settings.db'}")
+    return temp / "settings.db"
+
+
+def change_language(default: str = "cz") -> dict:
+    with sqlite3.connect(settings_file()) as dbcon:
+        df = pd.read_sql_query("SELECT * FROM language;", dbcon)
+    return dict(zip(df["col"], df[default]))
+
+
+if __name__ == "__main__":
+    t = change_language("cz")
+    print(t["display"])
