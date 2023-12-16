bd9b1c73235a09840dcab79bff19c7aaea91fa5b
kubow <kube.kubow@gmail.com>
2023-12-16 21:07:54 +0100

    added tests for date and place

--- tests/dates_places.py
+++ tests/dates_places.py
@@ -4,11 +4,17 @@ from context import Actual
 
 class Context(unittest.TestCase):
     def test_current_timestamp(self):
-        self.assertEqual(Actual(), datetime.now())
+        self.assertEqual(Actual().value, datetime.now())
 
-    def test_location_fallback(self):
-        self.assertEqual(Actual(t="place"), "Prague")
+    def test_custom_timestamp(self):
+        self.assertEqual(Actual("11.1.2011").value, datetime(2011, 1, 11))
 
+    def test_no_location_fallback(self):
+        self.assertEqual(Actual(t="place".value), "Prague")
+
+    def test_false_location_fallback(self):
+        self.assertEqual(Actual("Blabla", t="place").value, "Prague")
+    
     # - date inputs check also
     # - as well as various places
     # - add negative time
